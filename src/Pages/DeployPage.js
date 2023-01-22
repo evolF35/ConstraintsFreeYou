e658c6126aac6e51500760860648a09df4c0e95f
evolF35 <maccyair@gmail.com>
2023-01-22 13:26:29 +0400

    constraints

--- src/Pages/DeployPage.js
+++ src/Pages/DeployPage.js
@@ -1,7 +1,11 @@
-
-
 import React, {useState} from 'react'
 import {ethers}  from 'ethers'
 
+const DeployPageJS = () => {
 
+    
+
+}
+
+export default DeployPageJS
 
--- src/Pages/PoolPage.js
+++ src/Pages/PoolPage.js
@@ -1,7 +1,12 @@
 
-
 import React, {useState} from 'react'
 import {ethers}  from 'ethers'
 
+const PoolPageJS = () => {
 
+    
+
+}
+
+export default PoolPageJS
 
